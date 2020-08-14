9211d4bf08af41b2ef6dd8226f32abd30ffdc1ba
LoboArkano <pipiolo_mananes@hotmail.com>
2020-08-14 17:29:05 -0500

    Create gameBoard Module

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,28 @@
+const boardContainer = document.getElementById('game-board');
+
 const player = (name, score) => {
   const getName = () => name;
   const getScore = () => score;
   const updateScore = () => { score += 1; };
   return { getScore, getName, updateScore };
 };
+
+const gameBoard = (() => {
+  const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+
+  const display = () => {
+    boardContainer.innerHTML = '';
+
+    board.forEach((cell) => {
+      const cellBoard = `
+      <div class="cell" value="${cell}">${cell}</div>
+      `;
+
+      boardContainer.innerHTML += cellBoard;
+    });
+  };
+
+  return { display };
+})();
+
+gameBoard.display();

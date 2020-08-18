3307375887ef4b94cc4c47c2106e2291e58a0351
LoboArkano <pipiolo_mananes@hotmail.com>
2020-08-18 15:02:56 -0500

    Create gameEngine module
    Add event listener to receive the form data

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,11 @@
 /* eslint-disable prefer-destructuring */
 const boardContainer = document.getElementById('game-board');
 const mainController = document.getElementById('main-controller');
+const form = document.getElementById('users-form');
+const submitBtn = document.getElementById('ok-form');
+const usernNme1 = document.getElementById('username1');
+const usernNme2 = document.getElementById('username2');
+let players = [];
 
 const board = ['', '', '', '', '', '', '', '', ''];
 const marks = ['X', 'O'];
@@ -15,6 +20,19 @@ const player = (name, score, mark) => {
 };
 
 const gameBoard = (() => {
+  const updateCell = (cell) => {
+    if (cell.innerHTML === '') {
+      const index = cell.getAttribute('value');
+
+      board[index] = marks[0];
+      const temp = marks[0];
+      marks[0] = marks[1];
+      marks[1] = temp;
+    } else {
+      alert('This space has been taken');
+    }
+  };
+
   const render = () => {
     let i = 0;
     boardContainer.innerHTML = '';
@@ -31,16 +49,7 @@ const gameBoard = (() => {
     const cells = boardContainer.querySelectorAll('.cell');
     cells.forEach((cell) => {
       cell.addEventListener('click', () => {
-        const index = cell.getAttribute('value');
-
-        if (cell.innerHTML === '') {
-          board[index] = marks[0];
-          const temp = marks[0];
-          marks[0] = marks[1];
-          marks[1] = temp;
-        } else {
-          alert('This space has been taken');
-        }
+        updateCell(cell);
 
         render();
       });
@@ -50,12 +59,21 @@ const gameBoard = (() => {
   return { render };
 })();
 
+
+function showForm() {
+  form.classList.remove('d-none');
+}
+
+function hideForm() {
+  form.classList.add('d-none');
+}
+
 const displayController = (() => {
   const start = () => {
     mainController.innerHTML = '';
 
     const startBtn = `
-    <div class="start-btn btn1">start</div>
+    <div class="start-btn btn1" onclick="showForm()">start</div>
     `;
 
     mainController.innerHTML = startBtn;
@@ -75,11 +93,34 @@ const displayController = (() => {
   return { start, gameOver };
 })();
 
-const playerOne = player('Lobo', 0, 'X');
-const playerTwo = player('Arcano', 0, 'Y');
+const gameEngine = (() => {
 
-console.log(playerOne.mark);
-console.log(playerTwo.mark);
+  const getUserData = () => {
 
-gameBoard.render();
-displayController.start();
+  };
+
+  const gameOn = () => {
+    gameBoard.render();
+    displayController.start();
+  };
+
+  return { gameOn };
+})();
+
+submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  players = [];
+  const playerOne = player(usernNme1.value, 0, 'X');
+  const playerTwo = player(usernNme2.value, 0, 'Y');
+
+  players.push(playerOne, playerTwo);
+  console.log(players[0].getName());
+
+  usernNme1.value = '';
+  usernNme2.value = '';
+  hideForm();
+  gameBoard.render();
+});
+
+gameEngine.gameOn();
